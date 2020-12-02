43d8fdb12f8de558e853212388a750096368b96d
frankx13 <frankx13580@gmail.com>
2020-12-03 00:23:35 +0100

    Added an admin interface and create super user plus entry model

--- learning_log/learning_logs/admin.py
+++ learning_log/learning_logs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Topic, Entry
+
 # Register your models here.
+# http://localhost:8000/admin/
+admin.site.register(Topic)
+admin.site.register(Entry)
